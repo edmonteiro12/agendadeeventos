59e3b6042f8fe83b338cf9637136b77e223a5967
edmonteiro12 <88411300+edmonteiro12@users.noreply.github.com>
2026-06-23 14:18:14 -0300

    Add files via upload

--- jobin.js
+++ jobin.js
@@ -6,7 +6,13 @@ const SYNC_CONFIG = {
     // Bin ID - está na URL: https://jsonbin.io/v3/b/[AQUI_O_BIN_ID]
     binId: '6a3abba2da38895dfef2cd4d' // Substitua pelo ID real do seu bin
 };
-
+// O sistema faz isso automaticamente
+fetch('https://api.jsonbin.io/v3/b/6a3abba2da38895dfef2cd4d', {
+    headers: {
+        'X-Master-Key': '$2a$10$PpKSqwwVFrJrZF8njCvPtOIbqETvjIvkjGBYu97curmyuop7iEBle',
+        'Content-Type': 'application/json'
+    }
+})
 // Exportar para uso em outros arquivos
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = SYNC_CONFIG;
