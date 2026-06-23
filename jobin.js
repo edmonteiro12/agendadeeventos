f864e5fec6e4007eadd38ef140dee63872a46c00
edmonteiro12 <88411300+edmonteiro12@users.noreply.github.com>
2026-06-23 11:12:43 -0300

    Add files via upload

---
+++ jobin.js
@@ -0,0 +1,5 @@
+const SYNC_CONFIG = {
+    useJsonBin: true,
+    jsonBinKey: '$2a$10$EpEPh3xp.yU3GEDpNhnv1ek5F.ScWjHSL876Hrx3KdE8P6rGudPQS',
+    binId: '$2a$10$PpKSqwwVFrJrZF8njCvPtOIbqETvjIvkjGBYu97curmyuop7iEBle'
+};
